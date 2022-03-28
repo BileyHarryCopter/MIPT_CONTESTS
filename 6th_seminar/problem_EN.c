8bc57ff2a60f7f48a5c2949fd91edda4ba130120
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-03-28 23:07:05 +0300

    6th

--- 6th_seminar/problem_EN.c
+++ 6th_seminar/problem_EN.c
@@ -1,34 +1,31 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #define MAX_ITER 4000
 #define accur 0.000001
 #define accur_strih 0.001
 #define pi 3.1415
 
-typedef struct koefs
-{
-    double a;
-    double b;
-    double c;
+typedef struct koefs {
+  double a;
+  double b;
+  double c;
 } koefs;
 
-double func (koefs kaef, double x)
-{
-    return ((kaef.a)*x*x*sin(x) + (kaef.b)*x*cos(x) + (kaef.c));
+double func(koefs kaef, double x) {
+  return ((kaef.a) * x * x * sin(x) + (kaef.b) * x * cos(x) + (kaef.c));
 }
 
-double func_shtrih (koefs kaef, double x)
-{
-    return (2*kaef.a - kaef.b)*x*sin(x) + (kaef.a * x*x + kaef.b)*cos(x);
+double func_shtrih(koefs kaef, double x) {
+  return (2 * kaef.a - kaef.b) * x * sin(x) +
+         (kaef.a * x * x + kaef.b) * cos(x);
 }
 
-double my_mod (double x)
-{
-    if (signbit (x))
-        return -x;
-    return x;
+double my_mod(double x) {
+  if (signbit(x))
+    return -x;
+  return x;
 }
 /*
 double newton_met (koefs kaef, double x)
@@ -43,34 +40,29 @@ double newton_met (koefs kaef, double x)
     return newton_met (kaef, x - (double)function / f_shtrih);
 }
 */
-double newton_met (koefs kaef, double x)
-{
-    int i = 0;
-    double f_shtrih, function = 0.0;
-    function = func (kaef, x);
-    f_shtrih = func_shtrih (kaef, x);
-    while (my_mod (function) > accur)
-    {
-        if (my_mod (f_shtrih) > accur_strih && i < MAX_ITER)
-        {
-            x -= (double)function / f_shtrih;
-            function = func (kaef, x);
-            f_shtrih = func_shtrih (kaef, x);
-        }
-        else
-            break;
-        i++;
-    }
-    return function;
+double newton_met(koefs kaef, double x) {
+  int i = 0;
+  double f_shtrih, function = 0.0;
+  function = func(kaef, x);
+  f_shtrih = func_shtrih(kaef, x);
+  while (my_mod(function) > accur) {
+    if (my_mod(f_shtrih) > accur_strih && i < MAX_ITER) {
+      x -= (double)function / f_shtrih;
+      function = func(kaef, x);
+      f_shtrih = func_shtrih(kaef, x);
+    } else
+      break;
+    i++;
+  }
+  return function;
 }
 
-int main (void)
-{
-    koefs kaef = {};
-    double x_init = 0.0;
-    scanf ("%lf %lf %lf %lf", &(kaef.a), &(kaef.b), &(kaef.c), &x_init);
+int main(void) {
+  koefs kaef = {};
+  double x_init = 0.0;
+  scanf("%lf %lf %lf %lf", &(kaef.a), &(kaef.b), &(kaef.c), &x_init);
 
-    printf ("%.5lf\n", newton_met (kaef, x_init));
+  printf("%.5lf\n", newton_met(kaef, x_init));
 
-    return 0;
+  return 0;
 }
--- 6th_seminar/problem_RP.c
+++ 6th_seminar/problem_RP.c
@@ -1,40 +1,64 @@
 #include <fenv.h>
-#include <math.h>
 #include <float.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main (void)
-{
-    float roof, cell = 0.0;
-    float frac, frac_up, frac_down = 0.0;
-    float pos_infty = 1 / 0;
-    float neg_infty = -1 / 0;
-    unsigned u_eps  = 0;
-    unsigned u_frac = 0;
-    unsigned u_up   = 0;
-    unsigned u_down = 0;
-    scanf ("%f %f", &roof, &cell);
+void hashmap_ctor(hashmap *hshmp, unsigned len_buff) {
+  char symb = 0;
+  char *str = 0;
+  unsigned ip = 0;
+  unsigned key = 0;
 
-    frac    = (float) roof / cell;
+  hshmp->size = SIZE;
+  hshmp->array = (node_t **)calloc(SIZE, sizeof(node_t *));
 
-    fesetround (FE_DOWNWARD);
+  for (ip = 0; ip < SIZE; ip++) {
+    hshmp->array[ip] = (node_t *)calloc(1, sizeof(node_t));
+  }
 
-    frac_up   = nextafterf (frac, pos_infty);
-    frac_down = nextafterf (frac, neg_infty);
+  symb = getchar();
 
-    memcpy (&u_frac, &frac, sizeof (float));
-    memcpy (&u_up, &frac_up, sizeof (float));
-    memcpy (&u_down, &frac_down, sizeof (float));
+  for (ip = 1; ip < len_buff; ip++) {
+    symb = getchar();
+    if (isalpha(symb) != 0) {
+      str = str_ctor(symb);
+      ip += strlen(str);
+      key = hash_calc(str);
+      hashmap_insrt(hshmp, key, str);
+      free(str);
+    }
+  }
+}
 
-    /*
-    printf ("%.20f\t%x\n", frac, u_frac);
-    printf ("%.20f\t%x\n", frac_up, u_up);
-    printf ("%.20f\t%x\n", frac_down, u_down);
-    */
+int main(void) {
+  float roof, cell = 0.0;
+  float frac, frac_up, frac_down = 0.0;
+  float pos_infty = 1 / 0;
+  float neg_infty = -1 / 0;
+  unsigned u_eps = 0;
+  unsigned u_frac = 0;
+  unsigned u_up = 0;
+  unsigned u_down = 0;
+  scanf("%f %f", &roof, &cell);
 
-    
+  frac = (float)roof / cell;
 
-    return 0;
+  fesetround(FE_DOWNWARD);
+
+  frac_up = nextafterf(frac, pos_infty);
+  frac_down = nextafterf(frac, neg_infty);
+
+  memcpy(&u_frac, &frac, sizeof(float));
+  memcpy(&u_up, &frac_up, sizeof(float));
+  memcpy(&u_down, &frac_down, sizeof(float));
+
+  /*
+  printf ("%.20f\t%x\n", frac, u_frac);
+  printf ("%.20f\t%x\n", frac_up, u_up);
+  printf ("%.20f\t%x\n", frac_down, u_down);
+  */
+
+  return 0;
 }

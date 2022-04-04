9ef557b459945a103851f734eb50b1e105c2c53e
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-04 22:53:00 +0300

    working 2nd part of virtual assembler

--- asm/problem_AS2.c
+++ asm/problem_AS2.c
@@ -4,17 +4,18 @@
 #include <ctype.h>
 #include <math.h>
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-                //    initialisation of enums for lexems    //
+//    initialisation of enums for lexems    //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-#define KRIT_KF 0.6
-#define ENCR_KF 2
-#define CAP_INIT 10
-#define STRSIZE 10
-
 enum errors
 {
     NO_ERROR = 0,
     ERROR
+};
+
+enum op_flags
+{
+    UNAR = 1,
+    BINAR = 2
 };
 
 typedef enum lexem_kind
@@ -57,12 +58,15 @@ static const char *op_name [OPLAST] =
     "OPLAST"            //      cringe moment
 };
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-                            //  for bit fields  //
+//  for bit fields  //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 #define A_MASK 0        //
 #define B_MASK 1        //      for
 #define C_MASK 2        //          registrs
 #define D_MASK 3        //
+#define UNAR_MASK 3     //
+#define BINAR_MASK 12   //
+
 
 #define MOVI_MASK 0     //
 #define ADD_MASK  128   //      for
@@ -72,52 +76,211 @@ static const char *op_name [OPLAST] =
 #define IN_MASK   192   //
 #define OUT_MASK  196   //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-typedef union lexem
-{
-    registers      regr;
-    operators      oper;
-    int            data;
-}   lexem;
-
-typedef struct lexem_t
-{
-    lexem_kind     kind;
-    lexem          lexm;
-}   lexem_t;
-
-typedef struct lex_array_t
-{
-    lexem_t        *lexems;
-    unsigned       capacity, size;
-} lex_array_t;
-
-//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-                        //  function's definitions  //
+//  function's definitions  //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 int RegrDtct (char regr);
 int DataCtor (char first);
 int OperDtct (char *oper);
 char *OperCtor (char first);
-int LexsResz (lex_array_t *lexus);
-int LexsFill (lex_array_t *lexus);
-int LexsPrint (lex_array_t *lexus);
-int LexsDelete (lex_array_t *lexus);
-int LexsPrintBin (lex_array_t * lexus);
-lex_array_t *LexsInit (unsigned capacity);
 int istrcmp (char * str1, const char * str2);
-int LexsInsrt (lex_array_t *lexus, int kind, int val);
+
+
+int ParceBin (int machine_word);
+int ReadBinTransAsm (FILE * file);
+int BinCtor (FILE * file, int first);
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 int main (void)
 {
-    char val = 0;
-
+    ReadBinTransAsm (stdin);
+    return 0;
+}
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+int ReadBinTransAsm (FILE * file)
+{
+    int val = 0;
     while (val != EOF)
     {
-        scanf ("%#x", &val);
-        printf ("val = %d\n", val);
+        val = getc (file);
+        if (isspace (val))
+            continue;
+        else if (val == '0')
+        {
+            val = getc (file);
+            if (val != 'x' && val != 'X')
+            {
+                printf ("ERROR\n");
+                return ERROR;
+            }
+            val = getc (file);
+            val = BinCtor (file, val);
+            ParceBin (val);
+        }
     }
 
-    return 0;
+    return NO_ERROR;
+}
+
+int BinCtor (FILE * file, int first)
+{
+    int val  = 0;
+    int symb = getc (file);
+
+    if (isspace (symb))
+    {
+        if (first >= 'a' && first <= 'f')
+            val += first - 'a' + 10;
+        else if (isdigit (first))
+            val += first - '0';
+        else
+            return -1;
+        return val;
+    }
+
+    if (first >= 'a' && first <= 'f')
+    {
+        val += 16 * (first - 'a' + 10);
+    }
+    else if (isdigit (first))
+        val += 16 * (first - '0');
+    else
+        return -1;
+
+    if (symb >= 'a' && symb <= 'f')
+        val += symb - 'a' + 10;
+    else if (isdigit (symb))
+        val += symb - '0';
+    else
+        return val;
+
+    return val;
+}
+
+int ParceBin (int machine_word)
+{
+    int val  = 0;
+    int oper = 0;
+    int op_flag = BINAR;
+    if (!(machine_word >> 7))
+    {
+        printf ("%s ", op_name[MOVI]);
+        val = machine_word;
+        printf ("%d\n", val);
+    }
+    else
+    {
+        oper |= machine_word >> 4;
+        switch (oper)
+        {
+            case ADD_MASK >> 4:
+                printf ("%s ", op_name[ADD - ADD + 1]);
+                break;
+            case SUB_MASK >> 4:
+                printf ("%s ", op_name[SUB - ADD + 1]);
+                break;
+            case MUL_MASK >> 4:
+                printf ("%s ", op_name[MUL - ADD + 1]);
+                break;
+            case DIV_MASK >> 4:
+                printf ("%s ", op_name[DIV - ADD + 1]);
+                break;
+            case IN_MASK >> 4:
+                //  nothing to write because of future cheking  //
+                op_flag = UNAR;
+                break;
+            default:
+                printf ("ERROR\n");
+                return ERROR;
+        }
+        if (op_flag == UNAR)
+        {
+            //  work with IN / OUT command
+            oper = machine_word >> 2;
+            switch (oper)
+            {
+                case IN_MASK >> 2:
+                    printf ("%s ", op_name[IN - ADD + 1]);
+                    break;
+                case OUT_MASK >> 2:
+                    printf ("%s ", op_name[OUT - ADD + 1]);
+                    break;
+                default:
+                    printf ("ERROR\n");
+                    return ERROR;
+            }
+        }
+
+        val = machine_word;
+        if (op_flag == UNAR)
+        {
+            switch (val & UNAR_MASK)
+            {
+                case A_MASK:
+                    printf ("A\n");
+                    break;
+                case B_MASK:
+                    printf ("B\n");
+                    break;
+                case C_MASK:
+                    printf ("C\n");
+                    break;
+                case D_MASK:
+                    printf ("D\n");
+                    break;
+                default:
+                    printf ("ERROR\n");
+                    return ERROR;
+            }
+        }
+        else if (op_flag == BINAR)
+        {
+            val &= BINAR_MASK;
+            switch (val >> 2)
+            {
+                case A_MASK:
+                    printf ("A, ");
+                    break;
+                case B_MASK:
+                    printf ("B, ");
+                    break;
+                case C_MASK:
+                    printf ("C, ");
+                    break;
+                case D_MASK:
+                    printf ("D, ");
+                    break;
+                default:
+                    printf ("ERROR\n");
+                    return ERROR;
+            }
+            val = machine_word;
+            val &= UNAR_MASK;
+            switch (val)
+            {
+                case A_MASK:
+                    printf ("A\n");
+                    break;
+                case B_MASK:
+                    printf ("B\n");
+                    break;
+                case C_MASK:
+                    printf ("C\n");
+                    break;
+                case D_MASK:
+                    printf ("D\n");
+                    break;
+                default:
+                    printf ("ERROR\n");
+                    return ERROR;
+            }
+        }
+        else
+        {
+            printf ("ERROR\n");
+            return ERROR;
+        }
+    }
+
+    return NO_ERROR;
 }
